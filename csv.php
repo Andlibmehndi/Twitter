a8114dfb2d88365c1432611a87e5720f363f0d5b
ANDLIBMEHNDI NAQVI <31627825+Andlibmehndi@users.noreply.github.com>
2017-09-21 13:22:03 +0530

    Add files via upload

---
+++ csv.php
@@ -0,0 +1,23 @@
+<?php 
+
+require('Functions.php');
+$function = new Functionality();
+
+if(isset($_REQUEST['download-csv'])){
+    //ob_clean();
+    //$filename='image/temp.csv';
+     $f = fopen('php://memory', 'w');
+    $screen_name = $_REQUEST['username'];
+    $tweets = $function->generatePDFTweet($screen_name);
+    $delimiter=",";
+    fputcsv($f, array("id", "createdAt"), $delimiter);
+    foreach ($tweets as $tweet) {
+        fputcsv($f, $tweet, $delimiter);
+    }
+    fseek($f, 0);
+    header('Content-Type:application/csv');
+    header("Content-Disposition:attachment;filename=".$screen_name.".csv");
+    fpassthru($f);
+
+}
+?>
